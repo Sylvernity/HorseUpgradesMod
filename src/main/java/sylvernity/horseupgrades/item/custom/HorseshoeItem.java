d894dc6af015d913697712ee3565430972f5decb
Sylvernity <44215344+Sylvernity@users.noreply.github.com>
2023-01-11 23:55:39 -0500

    Added Parchment parameter mappings Added Netherite, Diamond, Iron, and Gold horseshoe items/textures/JSON files.

---
+++ src/main/java/sylvernity/horseupgrades/item/custom/HorseshoeItem.java
@@ -0,0 +1,17 @@
+package sylvernity.horseupgrades.item.custom;
+
+import net.minecraft.world.entity.ai.attributes.Attributes;
+import net.minecraft.world.entity.animal.horse.Horse;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.HorseArmorItem;
+
+public class HorseshoeItem extends HorseArmorItem {
+
+    public HorseshoeItem(int pProtection, String pIdentifier, Properties pProperties) {
+        super(pProtection, pIdentifier, pProperties);
+        //AttributeModifier modifier = new AttributeModifier();
+        //Horse.getAttribute(Attributes.MOVEMENT_SPEED).getBaseValue();
+        Player.Armor
+        //Horse.getSpeed(5.5);
+    }
+}
