16e53bee70b50218815a858d13a230b0d06c59d7
jadac <jadacumber@aol.com>
2023-01-13 21:34:56 -0500

    Removed wooden horseshoe and updated mod icon

--- src/main/java/sylvernity/horseupgrades/item/ModItems.java
+++ src/main/java/sylvernity/horseupgrades/item/ModItems.java
@@ -13,7 +13,6 @@ import sylvernity.horseupgrades.item.custom.HorseshoeItem;
 public class ModItems {
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, HorseUpgrades.MODID);
 
-    public static final RegistryObject<Item> WOODEN_HORSESHOE = ITEMS.register("wooden_horseshoe", () -> new HorseshoeItem(0, "wooden_horseshoe" /*new ResourceLocation("horseupgrades", "models/item/wooden_horseshoe.png")*/, (new Item.Properties().stacksTo(1).tab(CreativeModeTab.TAB_MISC)), ArmorMaterials.LEATHER));
     public static final RegistryObject<Item> IRON_HORSESHOE = ITEMS.register("iron_horseshoe", () -> new HorseshoeItem(0, "iron_horseshoe" /*new ResourceLocation("horseupgrades", "models/item/iron_horseshoe.png")*/, (new Item.Properties().stacksTo(1).tab(CreativeModeTab.TAB_MISC)), ArmorMaterials.IRON));
     public static final RegistryObject<Item> GOLDEN_HORSESHOE = ITEMS.register("golden_horseshoe", () -> new HorseshoeItem(0, "golden_horseshoe" /*new ResourceLocation("horseupgrades", "models/item/golden_horseshoe.png")*/, (new Item.Properties().stacksTo(1).tab(CreativeModeTab.TAB_MISC)), ArmorMaterials.GOLD));
     public static final RegistryObject<Item> DIAMOND_HORSESHOE = ITEMS.register("diamond_horseshoe", () -> new HorseshoeItem(0, "diamond_horseshoe" /*new ResourceLocation("horseupgrades", "models/item/diamond_horseshoe.png")*/, (new Item.Properties().stacksTo(1).tab(CreativeModeTab.TAB_MISC)), ArmorMaterials.DIAMOND));
