491d8a93da94dc0676c882d10cd49f2a2e1d2e51
jadac <jadacumber@aol.com>
2023-01-22 14:52:49 -0500

    Made Horseshoe Anvil breakable by iron level picks and above

--- src/main/java/sylvernity/horseupgrades/data/tags/ModTagsProvider.java
+++ src/main/java/sylvernity/horseupgrades/data/tags/ModTagsProvider.java
@@ -2,24 +2,21 @@ package sylvernity.horseupgrades.data.tags;
 
 import net.minecraft.core.Registry;
 import net.minecraft.data.DataGenerator;
-import net.minecraft.data.tags.TagsProvider;
+import net.minecraft.data.tags.BlockTagsProvider;
 import net.minecraft.tags.BlockTags;
-import net.minecraft.tags.TagKey;
 import sylvernity.horseupgrades.block.ModBlocks;
 
-public class ModTagsProvider<T> extends TagsProvider<T> {
+public class ModTagsProvider<T> extends BlockTagsProvider {
 
-    public ModTagsProvider(DataGenerator pGenerator, Registry<T> pRegistry, String modId) {
-        super(pGenerator, pRegistry, modId, null);
-
+    public ModTagsProvider(DataGenerator pGenerator, Registry<T> pRegistry, String modId){
+        super(pGenerator);
     }
 
     @Override
     protected void addTags() {
-        T anvil = (T) ModBlocks.HORSESHOE_ANVIL;
-        this.tag((TagKey<T>) BlockTags.NEEDS_IRON_TOOL).add(anvil);
-        this.tag((TagKey<T>) BlockTags.MINEABLE_WITH_PICKAXE).add(anvil);
-        this.tag((TagKey<T>) BlockTags.ANVIL).add(anvil);
+        this.tag(BlockTags.NEEDS_IRON_TOOL).add(ModBlocks.HORSESHOE_ANVIL.get());
+        this.tag(BlockTags.MINEABLE_WITH_PICKAXE).add(ModBlocks.HORSESHOE_ANVIL.get());
+        this.tag(BlockTags.ANVIL).add(ModBlocks.HORSESHOE_ANVIL.get());
         // Adds an object to the tag
     }
 }
