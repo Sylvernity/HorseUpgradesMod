0833801cab00746044e1a8bb542f6478f20fa0ef
sylvernity <sylvernity@sylvernity.com>
2026-08-12 00:44:07 -0400

    Fixed rendering of HorseshoeItem on Horses

--- mod-neoforge/src/main/java/com/sylvernity/horseupgrades/item/custom/HorseshoeItem.java
+++ mod-neoforge/src/main/java/com/sylvernity/horseupgrades/item/custom/HorseshoeItem.java
@@ -10,12 +10,22 @@ import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.ArmorMaterial;
 import net.minecraft.world.item.AnimalArmorItem;
 import net.minecraft.world.item.Item;
+import org.jetbrains.annotations.Nullable;
 
 public class HorseshoeItem extends AnimalArmorItem {
+    private ResourceLocation resourceLocation;
+
     public HorseshoeItem(String pIdentifier, Item.Properties pProperties, Holder<ArmorMaterial> material){
-        this(0, ResourceLocation.fromNamespaceAndPath("horseupgrades", "textures/entity/horseshoe/" + pIdentifier + ".png"), pProperties, material);
+        this(ResourceLocation.fromNamespaceAndPath("horseupgrades", "textures/entity/horseshoe/" + pIdentifier + ".png"), pProperties, material);
     }
-    public HorseshoeItem(int pProtection, ResourceLocation pIdentifier, Item.Properties pProperties, Holder<ArmorMaterial> pMaterial){
+    public HorseshoeItem(ResourceLocation pIdentifier, Item.Properties pProperties, Holder<ArmorMaterial> pMaterial){
         super(pMaterial, BodyType.EQUESTRIAN, true, pProperties);
+        this.resourceLocation = pIdentifier;
     }
+
+    @Override
+    public @Nullable ResourceLocation getTexture() {
+        return this.resourceLocation;
+    }
+
 }
